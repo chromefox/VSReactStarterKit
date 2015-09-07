240a2466a078d462331eb9a22f0a58ccd7f045b6
Ronny Paulus Muliawan <ronny.muliawan@resolutelabs.com>
2015-09-07 16:59:06 +0800

    Added an initial Experimental MVC Web Project.

---
+++ ExperimentWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ExperimentWeb.Startup))]
+namespace ExperimentWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
