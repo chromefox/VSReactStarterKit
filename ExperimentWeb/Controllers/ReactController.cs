f8c1333fee5357383f4c3747dfd5783c95f54d5a
Ronny Paulus Muliawan <ronny.muliawan@resolutelabs.com>
2015-11-27 16:08:43 +0800

    Added a test case to demonstrate the usefulness of React.

--- ExperimentWeb/Controllers/ReactController.cs
+++ ExperimentWeb/Controllers/ReactController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ExperimentWeb.Models;
+using Newtonsoft.Json;
 
 namespace ExperimentWeb.Controllers
 {
@@ -12,6 +14,18 @@ namespace ExperimentWeb.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateComment(Comment comment)
+        {
+            // parse the comments object
+            var path = Server.MapPath("/data.json");
+            var curCommentList = JsonConvert.DeserializeObject<List<Comment>>(System.IO.File.ReadAllText(path));
+            curCommentList.Add(comment);
+            var newContent = JsonConvert.SerializeObject(curCommentList);
+            System.IO.File.WriteAllText(path, newContent);
+            return Content(newContent, "application/json");
         }
     }
 }
---
+++ ExperimentWeb/Models/Comments.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace ExperimentWeb.Models
+{
+    public class Comment
+    {
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("author")]
+        public string Author { get; set; }
+
+        [JsonProperty("text")]
+        public string Text { get; set; }
+    }
+}
