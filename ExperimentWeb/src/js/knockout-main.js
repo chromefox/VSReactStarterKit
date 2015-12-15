e68fdf43606f5eaa268c454874ae04715d662af1
Ronny Paulus Muliawan <ronny.muliawan@resolutelabs.com>
2015-12-15 10:47:49 +0800

    Initial prototype of Leaflet API.

--- ExperimentWeb/src/js/knockout-main.js
+++ ExperimentWeb/src/js/knockout-main.js
@@ -133,3 +133,4 @@ $(document).ready(function () {
         "Totally believable testimonials on project homepage"
     ]), document.getElementById("example4"));
 });
+//# sourceMappingURL=knockout-main.js.map
