c14db1a3d72777f2b25dc9aeed74266879c691e2
Ronny Paulus Muliawan <ronny.muliawan@resolutelabs.com>
2015-12-29 15:26:09 +0800

    Added basic typescript integration with node CommonJS module invocations and browserify on Gulp.

---
+++ ExperimentWeb/src/js/ts-example/main-ts-example.ts
@@ -0,0 +1,12 @@
+﻿/// <reference path="ts-example-1.ts" />
+import Tsexample1 = require("./ts-example-1");
+
+class TypeScriptExampleMain
+{
+    constructor() {
+        // init all the different modules
+        new Tsexample1.Example1();
+    }
+}
+
+new TypeScriptExampleMain();
---
+++ ExperimentWeb/src/js/ts-example/ts-example-1.ts
@@ -0,0 +1,15 @@
+﻿import $ = require('jQuery');
+
+export class Example1 {
+    constructor() {
+        // initialize code on documentReady jQuery style
+        $(document).ready(this.init);
+    }
+
+    init() {
+        var idSelector = "#ts-example-1";
+        var idContent = "Hello Example 1";
+        $(idSelector).text(idContent);
+        $(idSelector).css("background", "red");
+    }
+}
