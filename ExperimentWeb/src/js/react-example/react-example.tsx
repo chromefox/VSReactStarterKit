24ffe5418f5d9826ceabe4c3b33c89e5fc435759
Ronny Paulus Muliawan <ronny.muliawan@resolutelabs.com>
2015-12-30 15:21:03 +0800

    Added content and components via React code.

--- ExperimentWeb/src/js/react-example/react-example.tsx
+++ ExperimentWeb/src/js/react-example/react-example.tsx
@@ -15,10 +15,55 @@ var AlertComponent = React.createClass({
     }
 });
 
-export class InfoBox {
-    domRender(domId: string, domText: string) {
+var TableComponent = React.createClass({
+    render() {
+        var rows = this.props.data.map(row => {
+            return (<tr>
+                <td>
+                    {row.name}
+                    </td>
+                    <td>
+                    {row.email}
+                        </td>
+                    <td>
+                    {row.year}
+                        </td>
+                </tr>);
+        });
+
+        return (
+            <table className="dataTable table table-bordered">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Year</th>
+                </tr>
+            </thead>
+        <tbody>
+            {rows}
+            </tbody>
+                </table>
+        );
+    }
+});
+
+var PageComponent = React.createClass({
+    render() {
+        return (
+            <div>
+                <AlertComponent text={this.props.alertText} />
+                <TableComponent data={this.props.tableData} />
+            </div>
+        );
+    }
+});
+
+
+export class LabsPage {
+    domRender(domId: string, domText: string, tableData: Object) {
         ReactDOM.render(
-            <AlertComponent text={domText} />,
+            <PageComponent alertText={domText} tableData={tableData} />,
             document.getElementById(domId)
         );
     }
--- ExperimentWeb/src/js/react-example/react-main.ts
+++ ExperimentWeb/src/js/react-example/react-main.ts
@@ -1,14 +1,44 @@
 ﻿// A '.tsx' file enables JSX support in the TypeScript compiler, 
 // for more information see the following page on the TypeScript wiki:
 // https://github.com/Microsoft/TypeScript/wiki/JSX
-
+declare var global: Global;
+global.$ = require('jquery');
+require('datatables.net-bs')();
 import reactEx1 = require("./react-example");
 
 class ReactExampleMain {
+    instruction: string = "Hello from React! The Datatable initialization and components are all created via React and JS." +
+    "For the purposes of demo, the onclick events are all attached from the React component itself." +
+    "For jQuery plugins integration, I think it is much safer to integrate the onclick via the plugin API.";
+
+    data: Array<Object> = [
+        {
+            name: "Alpha",
+            email: "Alpha@email.com",
+            year: "1989"
+        },
+        {
+            name: "Beta",
+            email: "Beta@email.com",
+            year: "2039"
+        },
+        {
+            name: "Charlie",
+            email: "Charlie@email.com",
+            year: "2736"
+        }
+    ];
+
+
     constructor() {
         // init all the different modules
-        var infobox = new reactEx1.InfoBox();
-        infobox.domRender("content", "Hello from React!.");
+        var infobox = new reactEx1.LabsPage();
+        infobox.domRender("content", this.instruction, this.data);
+        this.initLibs();
+    }
+
+    initLibs() {
+        $("table").DataTable();
     }
 }
 
